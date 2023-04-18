9c00cbd0d814c3f917f99b499f8f4688d7834e67
sreyaeve <121951124+sreyaeve@users.noreply.github.com>
2023-04-18 08:13:55 +0700

    Add files via upload

---
+++ other/about.sh
@@ -0,0 +1,34 @@
+NC='\e[0m'
+DEFBOLD='\e[39;1m'
+RB='\e[31;1m'
+GB='\e[32;1m'
+YB='\e[33;1m'
+BB='\e[34;1m'
+MB='\e[35;1m'
+CB='\e[35;1m'
+WB='\e[37;1m'
+clear
+echo ""
+echo ""
+echo -e "${RB}—————————————————————————————————————————————————————————${NC}"
+echo -e "                  ${GB}Xray Mini Premium${NC}"
+echo -e "${RB}—————————————————————————————————————————————————————————${NC}"
+echo -e "  ${GB}»»» Protocol Service «««  |  »»» Network Protocol «««${NC}  "
+echo -e "${RB}—————————————————————————————————————————————————————————${NC}"
+echo -e "  ${YB}- Vless${NC}                   ${GB}|${NC}  ${YB}- Websocket (CDN) non TLS${NC}"
+echo -e "  ${YB}- Vmess${NC}                   ${GB}|${NC}  ${YB}- Websocket (CDN) TLS${NC}"
+echo -e "  ${YB}- Trojan${NC}                  ${GB}|${NC}  ${YB}- gRPC (CDN) TLS${NC}"
+echo -e "  ${YB}- Socks5${NC}                  ${GB}|${NC}"
+echo -e "  ${YB}- Shadowsocks${NC}             ${GB}|${NC}"
+echo -e "  ${YB}- Shadowsocks 2022${NC}        ${GB}|${NC}"
+echo -e "${RB}————————————————————————————————————————————————————————${NC}"
+echo -e "               ${GB}»»» Network Port Service «««${NC}             "
+echo -e "${RB}————————————————————————————————————————————————————————${NC}"
+echo -e "  ${YB}- HTTPS : 443, 2053, 2083, 2087, 2096, 8443${NC}"
+echo -e "  ${YB}- HTTP  : 80, 8080, 8880, 2052, 2082, 2086, 2095${NC}"
+echo -e "${RB}————————————————————————————————————————————————————————${NC}"
+echo ""
+echo "This Original Script is from Dugong, Remake by ZenVPN"
+echo ""
+read -n 1 -s -r -p "Press any key to back on menu"
+menu
